b3ceced94762d114c03ab157af01437f77fffc30
zbahati <bahatizirimwabagabo@gmai.com>
2023-06-05 15:43:10 +0200

    Ft/Calculator
    - Add Component Calculator
    - Add create Child CalculatorBody
    - Add Child component CalculatorScreen
    - Add Child Componet OperatorSide
    
    [Finishes ..]

---
+++ src/components/CalculatorScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function CalculatorScreen() {
+  return (
+    <div>
+      <div className="calculator-screen">
+        <p>0</p>
+      </div>
+    </div>
+  );
+}
+
+export default CalculatorScreen;
