e5d3ab84acc93064ec88c33182cae33ebb8e7e0f
zbahati <bahatizirimwabagabo@gmai.com>
2023-06-07 16:13:08 +0200

    Ft quotes-api-call
    - Add state
    - Add error state
    - Add useEffect
    - call data with Fetch function
    - Call components to App.js
     - Style the quotes section
    - Loading state while fetch still fecthing data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import Calculator from './components/calculator';
+import Quotes from './components/quotes';
 
 function App() {
   return (
-    <div className="container">
-      <Calculator />
+    <div>
+      <div className="container">
+        <Calculator />
+      </div>
+      <div className="quotes">
+        <Quotes />
+      </div>
     </div>
   );
 }
